0e00c62a104460ea995add9aa1a116d5d4b9a691
Anurag <anuragkumar002004@gmial.com>
2026-08-13 16:44:56 +0530

    initial backend project setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+import {app} from './app.js';
+
+dotenv.config({
+    path: "./.env"
+});
+
+const PORT = process.env.PORT || 4000;
+
+connectDB()
+ .then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+ })
+ .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+ })
+
+ 

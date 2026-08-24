8e548c347f9fb71f202d129d60962931fa2cc753
Anurag <anuragkumar002004@gmail.com>
2026-08-24 12:08:59 +0530

    feat: implement user avatar upload functionality with error handling

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -250,7 +250,22 @@ return res
 })
 
 const updateUserAvatar = asyncHandler(async(req, res) => {
-  //
+  const avatar = await uploadCloudinary(avatarLocalPath)
+  if(!avatar.url){
+    throw new ApiError(500, "Something went wrong while uploading avatar")
+  }
+ const user = await User.findByIdAndUpdate(req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url
+      }
+    },
+    {new: true}
+  ).select("-password -refreshToken")
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar Updated successfully"))
 })
 
 const updateUserCoverImage = asyncHandler(async(req, res) => {

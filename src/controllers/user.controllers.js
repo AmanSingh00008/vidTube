e70c5364fe39221985505d9bc6b3440f989c0c81
Anurag <anuragkumar002004@gmail.com>
2026-08-24 11:47:33 +0530

    feat: add account details update functionality with validation for fullname and email

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -224,11 +224,29 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 })
 
 const getCurrentUser = asyncHandler(async(req, res) => {
-    //
+    res.status(200).json(new ApiResponse(200, req.user, "Current user Details"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
-  //
+  const {fullname, email} = req.body;
+
+  if(!fullname || !email){
+    throw new ApiError(400, "Fullname and email are required")
+  }
+
+ const user = await User.findByIdAndUpdate(req.user._id, {
+    $set: {
+      fullname,
+      email: email
+    }
+  },
+  {new: true}
+).select("-password -refreshToken")
+
+return res
+  .status(200)
+  .json(new ApiResponse(200, user, "Account deatils updated successfully"))
+
 })
 
 const updateUserAvatar = asyncHandler(async(req, res) => {

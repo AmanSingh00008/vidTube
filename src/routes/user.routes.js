72f25f7e0c1bed9c3bd679a30fe2e796b19c2d4b
Anurag <anuragkumar002004@gmail.com>
2026-08-25 15:28:51 +0530

    feat: add user authentication routes for login and token refresh

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
-import { registerUser, logoutUser } from "../controllers/user.controllers.js";
+import {
+  registerUser,
+  logoutUser,
+  loginUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getUserChannelProfile,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getWatchHistory
+} from "../controllers/user.controllers.js";
 import { Upload } from "./middlewares/multer.middleware.js";
-import {verifyJWT} from "../middlewares/auth.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router();
 router.route("/register").post(
   Upload.fields([
@@ -13,9 +26,19 @@ router.route("/register").post(
   ]),
   registerUser
 );
+router.route("/login").post(loginUser);
+router.route("/refreshAccessToken").post(refreshAccessToken);
 
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/change-password").post(changeCurrentPassword)
+router.route("/current-user").post(verifyJWT,getCurrentUser)
 
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
+router.route("/coverimage").patch(verifyJWT, upload.single("coverimage"),updateUserCoverImage)
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 export default router;
